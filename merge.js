71811f6e35a1462ccf916719fdd555143d1e0d7e
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-03-18 23:16:41 +0300

    Done

---
+++ merge.js
@@ -0,0 +1,14 @@
+function mergeStrings(first, second){
+    let count = 0;
+    
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] == second[count]) {
+        count++;
+      } else {
+        count = 0;
+        if (first[i] == second[count]) count++;
+      }
+    }
+  
+    return first + second.slice(count);
+  }
